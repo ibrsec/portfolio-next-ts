777c6f8c1e16575c8ca6cd3446dc6d98bd76b419
ibrahim seçkin <121899433+ibrsec@users.noreply.github.com>
2024-10-14 14:35:38 +0300

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,7 +11,7 @@ const AboutPage = () => {
               <div className="flex-grown">
                 <h3 className='text-7xl lg:text-8xl'>About <span className='text-[#64fa91]'>Me</span></h3>
                 <h4 className=' my-7 text-5xl lg:text-7xl'>Frontend Developer!</h4>
-                <p className='font-[400] my-7 text-2xl tracking-wide'>I'm a passionate and results-oriented Front-End Developer with a strong foundation in building user-centric web applications. My experience bridges the gap between software development and quality assurance, allowing me to deliver exceptional web experiences.</p>
+                <p className='font-[400] my-7 text-2xl tracking-wide'>I'm a passionate and results-oriented Full-Stack Developer with a strong foundation in building user-centric web applications. My experience spans both frontend and backend development, bridging the gap between software engineering and quality assurance. This unique blend allows me to deliver seamless, high-quality web solutions that prioritize both functionality and performance.</p>
                 <a className="" href="https://www.linkedin.com/in/ibr-seckin/" target="true">
                 <ButtonComp text="Read More" />
                 </a>
